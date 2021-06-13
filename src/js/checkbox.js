7bb16b5495eb87ba40dd407aa1ed026bb4ef7258
AleksDu <aleksandr.dyshkin@gmail.com>
2021-06-13 19:17:47 +0300

    Update checkbox.js

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -2,21 +2,21 @@ const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
+const bodyRef = document.querySelector('body');
 
-const ref = document.querySelector('body');
-function onChangeCheckbox(evt) {
+function onChangeCheckbox (evt) {
     if (evt.target.checked) {
-        ref.classList.add(Theme.DARK);
-        ref.classList.add(Theme.LIGHT);
+        bodyRef.classList.add(Theme.DARK);
+        bodyRef.classList.remove(Theme.LIGHT);
 
         localStorage.setItem('Theme', Theme.DARK);
+         
     } else if (!evt.target.checked) {
-        ref.classList.add(Theme.LIGHT);
-        ref.classList.add(Theme.DARK);
-
+        bodyRef.classList.add(Theme.LIGHT);
+        bodyRef.classList.remove(Theme.DARK);
+        
         localStorage.setItem('Theme', Theme.LIGHT);
     };
-
 }
 
 export {onChangeCheckbox};
